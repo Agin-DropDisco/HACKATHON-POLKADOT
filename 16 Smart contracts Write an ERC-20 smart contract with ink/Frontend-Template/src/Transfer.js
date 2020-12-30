65230dc51815a5581bfe0849063f09450c603ac2
Agin-DropDisco <76779267+Agin-DropDisco@users.noreply.github.com>
2020-12-30 21:06:24 +0700

    Add files via upload

---
+++ 16 Smart contracts Write an ERC-20 smart contract with ink/Frontend-Template/src/Transfer.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Form, Input, Grid, Label, Icon } from 'semantic-ui-react';
+import { TxButton } from './substrate-lib/components';
+
+export default function Main (props) {
+  const [status, setStatus] = useState(null);
+  const [formState, setFormState] = useState({ addressTo: null, amount: 0 });
+  const { accountPair } = props;
+
+  const onChange = (_, data) =>
+    setFormState(prev => ({ ...prev, [data.state]: data.value }));
+
+  const { addressTo, amount } = formState;
+
+  return (
+    <Grid.Column width={8}>
+      <h1> BURGER TOKEN </h1>
+      <Form>
+        <Form.Field>
+        <Label basic color='blue' pointing = 'left'>
+            <Icon name='fork' content='Fork'/>
+            {/* <Label pointing = 'left' basic = 'true' color ='blue'/> */}
+            1 Unit = 1000000000000
+          </Label>
+          {/* <Label basic color='teal'>
+            <Icon name='hand point right' />
+            1 Unit = 1000000000000
+          </Label> */}
+        </Form.Field>
+        <Form.Field>Transfer more than the existential amount for account with 0 balance</Form.Field>
+        <Form.Field>
+          <Input
+            fluid
+            label='Transfer To'
+            type='text'
+            placeholder='address'
+            state='addressTo'
+            onChange={onChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            fluid
+            label='Amount of Burger Token'
+            type='number'
+            state='amount'
+            onChange={onChange}
+          />
+        </Form.Field>
+        <Form.Field style={{ textAlign: 'center' }}>
+          <TxButton
+            accountPair={accountPair}
+            label='Submit'
+            type='SIGNED-TX'
+            setStatus={setStatus}
+            attrs={{
+              palletRpc: 'balances',
+              callable: 'transfer',
+              inputParams: [addressTo, amount],
+              paramFields: [true, true]
+            }}
+          />
+        </Form.Field>
+        <div style={{ overflowWrap: 'break-word' }}>{status}</div>
+      </Form>
+    </Grid.Column>
+  );
+}
---
+++ 16 Smart contracts Write an ERC-20 smart contract with ink/README.md
@@ -0,0 +1,25 @@
+# [INTERMEDIATE CHALLENGE] 
+# Smart Contracts - Write An ERC-20 Smart Contract With Ink!
+
+### Prof.of UI | Transfer Token From-To 
+- [x] https://github.com/burgerking12/Polkadot-Hello-World-Hackathon/tree/main/ERC20-With-INK/Picture
+###  Simple UI | Substrate Front End Template 
+- [x] https://github.com/burgerking12/Polkadot-Hello-World-Hackathon/tree/main/ERC20-With-INK/Frontend-Template
+###  Substrate Node Template | upadate RPC & more
+- [x] https://github.com/burgerking12/Polkadot-Hello-World-Hackathon/tree/main/ERC20-With-INK/Substrate-Node-Template
+###  Erc20 Smart Contract | BURGER TOKEN 
+- [x] https://github.com/burgerking12/Polkadot-Hello-World-Hackathon/tree/main/ERC20-With-INK/Burger-Token
+
+
+
+### Plugins
+
+| Plugin | Link |
+| ------ | ------ |
+| Subtrate | https://getsubstrate.io |
+| Node Js | https://nodejs.org |
+| Yarn | https://yarnpkg.com |
+| Rustup | https://rustup.rs |
+| Webpack |https://webpack.js.org |'
+
+* Ask [Agin DropDisco](https://twitter.com/agin_webdev) (A.K.A) Burgerking12 :)
